9df6a9d10ede4404e8f25d1b507ffe8f44e501f5
Юлия Бусыгина <ubusygina747@gmail.com>
2026-05-14 02:11:40 +0500

    Внесены изменения

--- src/product.py
+++ src/product.py
@@ -1,17 +1,15 @@
 class Product:
     """Класс для представления продуктов"""
-    name:str
-    description:str
-    price:float
-    quantity:int
 
+    name: str
+    description: str
+    price: float
+    quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         """Метод для инициализации экземпляра класса.
         Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-
